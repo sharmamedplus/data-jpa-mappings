568eac9e92812dac52eaa821cb77b9ee3b9c2aa7
Vijay Sharma <vijaysharma6760@gmail.com>
2024-05-31 17:15:05 +0530

    First commit

---
+++ src/main/java/com/jpa/mappings/DataJpaMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaMappingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/mappings/DataJpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.mappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
